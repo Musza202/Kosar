a8f2ab2dae108bc25f1856afb55ec82c065902bc
Musza202 <mtomika2020@gmail.com>
2025-02-04 10:09:37 +0100

    html_js_kesz

--- cart.js
+++ cart.js
@@ -3,6 +3,7 @@ let product = [
     {id: 2, name: "körte", price: 250, quantity: 1},
     {id: 3, name: "barack", price: 350, quantity: 1}
 ];
+
 function alma() {
     fetch("https://fakestoreapi.com/products")
         .then(x => x.json())
@@ -21,33 +22,77 @@ function render(adat) {
     });
 
     product = bele;  
+    console.log("Frissített termékek:", product);
+
+    
+    peldany.updateCart(product);
+
+    
     kiir(peldany); 
     renderProductList(); 
 }
 
+let ki = document.getElementById("kimenet-body");
+
 class cart {
     constructor(asd) {
-        this.items = asd; 
+        this.items = asd;
+    }
+
+    viewCart() {
+        console.table(this.items);
     }
 
     addProduct(productid, productname, productprice = 400, productquantity = 1) {
-        const cartitem = this.items.find(p => p.id == productid);
+        const cartitem = this.items.find(p => p.id == productid); 
         if (cartitem === undefined) {
-            this.items.push({id: productid, name: productname, price: productprice, quantity: productquantity});
+            this.items = [...this.items, {id: productid, name: productname, price: productprice, quantity: productquantity}];
+            console.log(`Termék hozzáadva: ${productname} - Mennyiség: ${productquantity}`);
         } else {
-            cartitem.quantity += productquantity;
+            for (let i = 0; i < this.items.length; i++) {
+                if (this.items[i].id == productid) {
+                    this.items[i].quantity += productquantity;
+                    console.log(`Termék frissítve: ${productname} - Új mennyiség: ${this.items[i].quantity}`);
+                }
+            }
         }
     }
 
-    updateProduct(productid, quantity) {
-        const cartitem = this.items.find(p => p.id == productid);
-        if (cartitem) {
-            cartitem.quantity = quantity;
+    removeProduct(productid) {
+        const cartitem = this.items.find(p => p.id == productid); 
+        if (cartitem === undefined) {
+            console.log("Nem található id!");
+        } else {
+            this.items = this.items.filter(item => item.id !== productid);
         }
     }
 
-    getItems() {
-        return this.items;
+    updateProduct(productid, productquantity) {
+        this.items = this.items.map(item => item.id == productid ? {...item, quantity: productquantity} : item);
+    }
+
+    cleanCart() {
+        this.items = [];
+    }
+
+    calTotal() {
+        let szamol = 0;
+        for (let i = 0; i < this.items.length; i++) {
+            szamol += this.items[i].price * this.items[i].quantity; 
+        }
+        console.log(szamol);
+    }
+
+    
+    updateCart(updatedProducts) {
+        this.items = updatedProducts.map(product => {
+            return { 
+                id: product.id, 
+                name: product.name, 
+                price: product.price, 
+                quantity: 1 
+            };
+        });
     }
 }
 
@@ -71,7 +116,7 @@ function kiir(asd) {
         tr.appendChild(priceCell);
         tr.appendChild(quantityCell);
         tbody.appendChild(tr);
-    };
+    }
 
     let regiTbody = document.getElementById("kimenet-body");
     regiTbody.parentNode.replaceChild(tbody, regiTbody);
@@ -82,26 +127,17 @@ function hozad(asd) {
     let p_nev = document.getElementById("nev").value;
     let p_ar = Number(document.getElementById("ar").value);
     let p_darab = Number(document.getElementById("darab").value);
-
-    const isProductInCart = asd.items.some(item => item.id === p_id);
     
     if (p_id && p_nev && p_ar && p_darab) {
-        if (!isProductInCart) {
-            asd.addProduct(p_id, p_nev, p_ar, p_darab);
-        } else {
-            alert("Ez a termék már a kosárban van!");
-        }
-
-        kiir(asd);
-        renderProductList(); 
-    } else {
-        alert("Hibás adatbevitel!");
+        asd.addProduct(p_id, p_nev, p_ar, p_darab);
     }
+
+    kiir(asd);
 }
 
 function renderProductList() {
     let productBody = document.getElementById("product-list-body");
-    productBody.innerHTML = "";
+    productBody.innerHTML = "";  
 
     for (let i = 0; i < product.length; i++) {
         let tr = document.createElement("tr");
@@ -114,24 +150,10 @@ function renderProductList() {
         arCell.innerText = product[i].price;
         kosarba.innerText = "Hozzáad";
         kosarba.classList.add("add-to-cart");
-
-        const isProductInCart = peldany.items.some(item => item.id === product[i].id);
-
-        if (isProductInCart) {
-            kosarba.disabled = false;
-            kosarba.innerText = "Hozzáadás";
-            kosarba.onclick = function () {
-                peldany.addProduct(product[i].id, product[i].name, product[i].price, 1);
-                kiir(peldany);
-                renderProductList(); 
-            };
-        } else {
-            kosarba.onclick = function () {
-                peldany.addProduct(product[i].id, product[i].name, product[i].price, 1);
-                kiir(peldany);
-                renderProductList();
-            };
-        }
+        kosarba.onclick = function() {
+            peldany.addProduct(product[i].id, product[i].name, product[i].price, 1);
+            kiir(peldany);
+        };
 
         vasarolCell.appendChild(kosarba);
         tr.appendChild(nevCell);
@@ -141,8 +163,8 @@ function renderProductList() {
     }
 }
 
-
-
+alma();
 
 const peldany = new cart(product);
-alma();
+kiir(peldany);
+renderProductList();
